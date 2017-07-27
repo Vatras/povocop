645c0b8e9287b36217b3653596073d700ccf7bc0
Vatras <fabisiak.tomek@gmailcom>
2017-07-27 14:19:29 +0200

    Tabs

--- scheduler/public/scripts/inputManager.js
+++ scheduler/public/scripts/inputManager.js
@@ -15,12 +15,39 @@ function generateData(){
   for(var i=startRange;i<endRange;i+=modulo){
     dataSet.push([idx++,i,false])
   }
-  createTable()
+  createTable(dataSet,'inputDataTable')
 }
-function createTable() {
-  $('#inputDataTable_wrapper').replaceWith('<table id="inputDataTable" class="display" width="100%"></table>')
-  $('#inputDataTable').DataTable( {
-    data: dataSet,
+window.onload= function(){
+  // openTab(event, 'addData')
+  document.getElementsByClassName('initial')[0].click();
+}
+
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+  if(tabName=='browseData'){
+    getInputData();
+  }
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+function createTable(data,tableName) {
+  $('#TABLENAME_wrapper'.replace('TABLENAME',tableName)).replaceWith('<table id="TABLENAME" class="display" width="100%"></table>'.replace('TABLENAME',tableName))
+  $('#TABLENAME'.replace('TABLENAME',tableName)).DataTable( {
+    data: data,
     columns: [
       { title: "No." },
       { title: "Value" },
@@ -41,7 +68,9 @@ function deleteInputData() {
 function getInputData() {
   var appname = window.location.pathname.split('/')[3]
   $.get("/data/"+ appname, function (data, status) {
-
+      console.log(data);
+      var parsedData = data.map(function(val,idx){return [idx,JSON.stringify(val.data),val.assigned]});
+      createTable(parsedData,'browseDataTable')
   });
 }
 function parseInputDate(dataSet){
